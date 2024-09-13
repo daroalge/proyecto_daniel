2fd5dc1a1b231f815e4bde53cdbb6db42a547c15
Daniel Alfonso <usdanielus26@gmail.com>
2024-09-12 20:47:08 -0500

    fixed dashboard and shop integration add styles for dashboardStyle.css

---
+++ src/products.js
@@ -0,0 +1,69 @@
+window.onload = (event) => {
+    //const encodeData = window.location.hash.substring(1);
+    //const data = JSON.parse(atob(encodeData));
+    //console.log(data);
+
+    loadProducts();
+
+    const createProducts = document.getElementById('createProducts');
+
+    createProducts.addEventListener('click', function(event){
+        window.location.href = './createProduct.html';
+        
+    });
+};
+
+async function loadProducts() {
+    try {
+        const response = await fetch('http://localhost:3000/product/list', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        const products = await response.json();
+        console.log(products)
+
+        const tableBody = document.getElementById('productsTbody');
+        tableBody.innerHTML = '';
+
+        products.forEach(product => {
+            const row = document.createElement('tr');
+
+            const idCell = document.createElement('td');
+            idCell.textContent = product.product_id;
+
+            const nombreHeladoCell = document.createElement('td');
+            nombreHeladoCell.textContent = product.nombre_helado;
+
+            const descripcionCell = document.createElement('td');
+            descripcionCell.textContent = product.descripcion;
+
+            const precioCell = document.createElement('td');
+            precioCell.textContent = product.precio;
+
+            const tamañoCell = document.createElement('td');
+            tamañoCell.textContent = product.tamaño;
+
+            const disponibilidadCell = document.createElement('td');
+            disponibilidadCell.textContent = product.disponibilidad;
+
+            const imagenCell = document.createElement('td');
+            imagenCell.textContent = product.imagen;
+
+            row.appendChild(idCell);
+            row.appendChild(nombreHeladoCell);
+            row.appendChild(descripcionCell);
+            row.appendChild(precioCell);
+            row.appendChild(tamañoCell);
+            row.appendChild(disponibilidadCell);
+            row.appendChild(imagenCell);
+
+
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error(error);  
+    }
+}
