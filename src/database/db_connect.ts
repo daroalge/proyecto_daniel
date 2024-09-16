90f3144bed700009b3317e620b5076424e7418ab
Daniel Alfonso <usdanielus26@gmail.com>
2024-09-16 07:39:52 -0500

    cambios frontend se quito y se djeo solo backend

--- src/database/db_connect.ts
+++ src/database/db_connect.ts
@@ -1,15 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
+import {Pool} from "pg";
 require('dotenv').config();
-let pool;
-if (process.env.PRODUCTION) {
-    pool = new pg_1.Pool({
+
+let pool: Pool;
+console.log(process.env.DB_DEV_PASSWORD)
+/*if (process.env.PRODUCTION) {
+    pool = new Pool({
         connectionString: process.env.POSTGRES_URL,
     });
-}
-else {
-    pool = new pg_1.Pool({
+} else {*/
+    pool = new Pool({
         host: process.env.DB_DEV_HOST,
         user: process.env.DB_DEV_USER,
         password: process.env.DB_DEV_PASSWORD,
@@ -17,5 +16,6 @@ else {
         port: parseInt(`${process.env.DB_DEV_PORT}`),
         idleTimeoutMillis: 3000,
     });
-}
-exports.default = pool;
+//}
+
+export default pool;
